96f901b99663d3447c01db99edcaafc1b8fd3db8
skiron14 <skiron14@gmail.com>
2022-09-19 23:48:57 +0530

    fourth commit

--- 1.2.JS 1 Introduction of JS/scripts.js
+++ 1.2.JS 1 Introduction of JS/scripts.js
@@ -2,6 +2,4 @@ console.log('This is coming from scripts.js')
 const h1 = document.querySelector('h1')
 console.log("This is my name kiron")
 
-
 h1.textContent = 'Custom Content is Here !!'
-console.log("This is my name kiron")
